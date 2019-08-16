17e56da26b76ccac78f48f017966ace914889398
Jared Tyler <jared@thalmic.com>
2019-08-16 18:56:49 -0400

    Move UI generation into service file.

--- bots/epsilon/epsilon.cmd.js
+++ bots/epsilon/epsilon.cmd.js
@@ -1,50 +1,7 @@
 
 const _ = require('lodash');
 const Slack = require('pico-slack');
-
-const Button = (text, style = 'danger') => {
-	return {
-		type: 'button',
-		text: { text, emoji: true, type: 'plain_text' },
-		style,
-	};
-};
-
-const Image = (image_url, alt_text = '') => {
-	return { image_url, alt_text, type: 'image' };
-};
-
-const Markdown = (text) => {
-	return { text, type: 'mrkdwn' };
-};
-
-const Dropdown = (action_id, options) => {
-	options = _.map(options, (text, value) => ({ value, text: { text, emoji: true, type: 'plain_text' } }));
-	return {
-		type: 'static_select',
-		action_id,
-		initial_option: _.first(options),
-		options,
-	};
-};
-
-const SectionBlock = (options) => {
-	return _.extend({ type: 'section' }, options);
-};
-
-const ActionsBlock = (elements) => {
-	if (!_.isArray(elements)) elements = [elements];
-	return { type: 'actions', elements };
-};
-
-const ContextBlock = (elements) => {
-	if (!_.isArray(elements)) elements = [elements];
-	return { type: 'context', elements };
-};
-
-const Divider = () => {
-	return { type: 'divider' };
-};
+const EpsilonService = require('./epsilon.service');
 
 module.exports = {
 	url    : '/epsilon',
@@ -55,56 +12,14 @@ module.exports = {
 
 		console.log(info);
 
-		const blocks = [
-			SectionBlock({
-				text      : Markdown("_What's your #1 choice?_"),
-				accessory : Dropdown('bleep', {
-					'<any>'         : "Don't Care",
-					'Captain'       : ':captain-2: Captain',
-					'Helms'         : ':helms: Helms',
-					'Weapons'       : ':weapons: Weapons',
-					'Engineering'   : ':engineering: Engineering',
-					'Science'       : ':science: Science',
-					'Relay'         : ':relay: Relay',
-					'Fighter Pilot' : ':fighter: Fighter Pilot',
-				}),
-			}),
-
-			Divider(),
-
-			SectionBlock({
-				text: Markdown("_Which roles are you *willing* to play, if you don't get your #1 choice?_"),
-			}),
-			ActionsBlock([
-				Button(':captain-2: Captain'),
-				Button(':helms: Helms'),
-				Button(':weapons: Weapons'),
-				Button(':engineering: Engineering'),
-				Button(':science: Science'),
-				Button(':relay: Relay'),
-				Button(':fighter: Fighter Pilot'),
-			]),
-
-			Divider(),
-
-			SectionBlock({
-				text      : Markdown('You have chosen to play as:\n*:captain-2: Captain*, *:weapons: Weapons*, *:relay: Relay*, or *:fighter: Fighter Pilot*\n(but ideally *:weapons: Weapons*)'),
-				accessory : Image('https://daid.github.io/EmptyEpsilon/images/logo.png', 'empty epsilon logo'),
-			}),
-
-			Divider(),
-
-			ActionsBlock(Button('ＲＥＡＤＹ  ＵＰ', 'primary')),
-			ContextBlock(Markdown('_Remember, you can always type `/epsilon` and edit your choices later!_')),
-		];
-		console.log('**message response', JSON.stringify(blocks, null, 2));
+//		console.log('**message response', JSON.stringify(blocks, null, 2));
 		return reply({
 //		Slack.api('chat.postMessage', {
 			channel    : info.channel_id,
 			username   : "Ship's Computer",
 			icon_emoji : ':rocket:',
 			text       : 'fallback text for notifications',
-			blocks,
+			blocks     : EpsilonService.buildUI(info.user_name),
 		});
 //		return res.status(200).send({ delete_original: true });
 		return reply();
---
+++ bots/epsilon/epsilon.service.js
@@ -0,0 +1,106 @@
+const _ = require('lodash');
+
+const Button = (text, value, style = 'danger') => {
+	return {
+		type: 'button',
+		value,
+		text: { text, emoji: true, type: 'plain_text' },
+		style,
+	};
+};
+
+const Image = (image_url, alt_text = '') => {
+	return { image_url, alt_text, type: 'image' };
+};
+
+const Markdown = (text) => {
+	return { text, type: 'mrkdwn' };
+};
+
+const Dropdown = (action_id, options, selected = null) => {
+	options = _.map(options, (text, value) => ({ value, text: { text, emoji: true, type: 'plain_text' } }));
+	const initial_option = selected ? _.find(options, { value: selected }) : _.first(options);
+	return {
+		type: 'static_select',
+		action_id,
+		initial_option,
+		options,
+	};
+};
+
+const SectionBlock = (options) => {
+	return _.extend({ type: 'section' }, options);
+};
+
+const ActionsBlock = (elements) => {
+	if (!_.isArray(elements)) elements = [elements];
+	return { type: 'actions', elements };
+};
+
+const ContextBlock = (elements) => {
+	if (!_.isArray(elements)) elements = [elements];
+	return { type: 'context', elements };
+};
+
+const Divider = () => {
+	return { type: 'divider' };
+};
+
+const ROLES = ['Captain', 'Helms', 'Weapons', 'Engineering', 'Science', 'Relay', 'Fighter Pilot'];
+
+const ROLE_LABELS = {
+	'Captain'       : ':captain-2: Captain',
+	'Helms'         : ':helms: Helms',
+	'Weapons'       : ':weapons: Weapons',
+	'Engineering'   : ':engineering: Engineering',
+	'Science'       : ':science: Science',
+	'Relay'         : ':relay: Relay',
+	'Fighter Pilot' : ':fighter: Fighter Pilot',
+};
+
+module.exports = {
+	buildUI(user) {
+		return [
+			SectionBlock({
+				text      : Markdown("_What's your #1 choice?_"),
+				accessory : Dropdown('bleep', {
+					'<any>'         : "Don't Care",
+					'Captain'       : ':captain-2: Captain',
+					'Helms'         : ':helms: Helms',
+					'Weapons'       : ':weapons: Weapons',
+					'Engineering'   : ':engineering: Engineering',
+					'Science'       : ':science: Science',
+					'Relay'         : ':relay: Relay',
+					'Fighter Pilot' : ':fighter: Fighter Pilot',
+				}),
+			}),
+
+			Divider(),
+
+			SectionBlock({
+				text: Markdown("_Which roles are you *willing* to play, if you don't get your #1 choice?_"),
+			}),
+			ActionsBlock([
+				Button(':captain-2: Captain', 'Captain'),
+				Button(':helms: Helms', 'Helms'),
+				Button(':weapons: Weapons', 'Weapons'),
+				Button(':engineering: Engineering', 'Engineering'),
+				Button(':science: Science', 'Science'),
+				Button(':relay: Relay', 'Relay'),
+				Button(':fighter: Fighter Pilot', 'Fighter Pilot'),
+			]),
+
+			Divider(),
+
+			SectionBlock({
+				text      : Markdown('You have chosen to play as:\n*:captain-2: Captain*, *:weapons: Weapons*, *:relay: Relay*, or *:fighter: Fighter Pilot*\n(but ideally *:weapons: Weapons*)'),
+				accessory : Image('https://daid.github.io/EmptyEpsilon/images/logo.png', 'empty epsilon logo'),
+			}),
+
+			Divider(),
+
+			ActionsBlock(Button('epsilon_commit', 'ＲＥＡＤＹ  ＵＰ', 'ok', 'primary')),
+			ContextBlock(Markdown('_Remember, you can always type `/epsilon` and edit your choices later!_')),
+		];
+	},
+};
